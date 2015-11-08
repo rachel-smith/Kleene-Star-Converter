045bdb3e6c48460fa9854a13eb813988355c1bef
Rachel Smith <smithra17@up.edu>
2015-11-07 17:07:20 -0800

    Gets all input from user and converts the NFA/DFA

--- Star/StarProject.java
+++ Star/StarProject.java
@@ -11,6 +11,15 @@ public class StarProject
     public String INTRO_PROMPT = "Welcome to the Kleene Star Converter Program!";
     public String Q_1 = "Please type NFA if converting an NFA or DFA if converting a DFA: ";
     static boolean validInput = true;
+    static String[] allStates = new String[21];
+    static String startState;
+    static String[] alphabet = new String[20];
+    static String[] acceptStates = new String[21];
+    
+    static Hashtable transitionTable = new Hashtable();
+        Enumeration names;
+        String str;
+        ArrayList transitions;
     /**
      * Constructor for objects of class StarProject
      */
@@ -23,36 +32,84 @@ public class StarProject
      * Main Method. Will handle entire program by calling 
      * helper functions and files.
      * 
-     * @author Rachel Smith and Nerissa Lemon
      */
     public static void main(String[] args){
         //Get user input by calling helper function; will check for valid input here.
         promptUser();
         
         //If user entered valid input, proceed.
-        if(validInput == true){
+        //if(validInput == true){
             //Perform computation of converting the NFA or DFA.
-
+            convert();
             //Return the appropriate NFA accepting A* to the user.
-        }
+       // }
 
     }
-
+    
+    /**
+     * Function convert()
+     * 
+     * Purpose: Convert given NFA or DFA to NFA that accepts A*
+     * 
+     */
+    public static void convert(){
+        //add new start state to the transitionTable
+        transitionTable.put("Start", new ArrayList<String>(Arrays.asList("epsilon->" + startState)));
+        //add new start state to list of accept states and to list of all states
+        int numAccept = countMyArray(acceptStates);
+        int numAll = countMyArray(allStates);
+        acceptStates[numAccept+1] = "Start";
+        allStates[numAll+1] = "Start";
+        startState = "Start";
+        System.out.println("*****************");
+        System.out.println("HERE IS THE GODDAMN ANSWER YOU LAZY PERSON");
+        System.out.println("Start State: "+startState);
+        System.out.println("Accept States: ");
+        System.out.println("Start");
+        printMyArray(acceptStates);
+        System.out.println("All States: ");
+        System.out.println("Start");
+        printMyArray(allStates);
+        System.out.println("Alphabet");
+        printMyArray(alphabet);
+        System.out.println("Transition Table: ");
+        String printShitOut = transitionTable.toString();
+        System.out.println(printShitOut);
+       
+    }
+    
+    public static void printMyArray(String[] arr){
+        int length = countMyArray(arr);
+        for(int i = 0; i < length; i++){
+            if(arr[i]!=null){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+    
+    /**
+     * Function propmtUser()
+     * 
+     * Purpose: Helper function to main, will prompt
+     *          user for info and store it appropriately
+     *          for later use and manipulation.
+     * 
+     */
+    
     public static void promptUser(){
         String INTRO_PROMPT = "Welcome to the Kleene Star Converter Program!";
         String Q_1 = "Please type NFA if converting a NFA or DFA if converting a DFA: ";
         String Q_2 = "Please type the start state(3 chars max): ";
         String Q_3 = "Please enter the Automata alphabet, by following the prompts. Max 20 chars, use 'empty' for empty string please. Otherwise, 1 char letters only, enter 'done' if done:";
         String Q_4 = "Please enter all accept states, one by one by folllowing the prompts";
+        String Q_5 = "Please enter all  states, one by one by folllowing the prompts";
         //Welcome user to program.
         System.out.println(INTRO_PROMPT);
         //Ask what they are converting.
         System.out.println(Q_1);
         //Save response, determine if prompt for NFA or DFA.
         String state;
-        String startState;
-        String[] alphabet = new String[20];
-        String[] acceptStates = new String[20];
+        //String[] allStates = new String[20];
         Scanner keyboard = new Scanner(System.in);
         state = keyboard.nextLine();
         state = state.trim();
@@ -67,7 +124,7 @@ public class StarProject
             System.out.println("So sorry, we can't convert anything of type: " + state + ". Program terminating.");
             validInput = false;
         }
-        
+        System.out.println();
         //Get start state. Must contain no spaces and only one word.
         if(validInput==true){
             System.out.println(Q_2);
@@ -85,7 +142,7 @@ public class StarProject
             }
             
         }
-        
+        System.out.println();
         //Get alphabet.
         if(validInput==true){
             System.out.println(Q_3);
@@ -106,30 +163,128 @@ public class StarProject
                 }
             }
         }
-        
+        System.out.println();
+        //Get all states.
+        if(validInput==true){
+            System.out.println(Q_5);
+            for( int i = 0; i < allStates.length; i++){
+                System.out.println("Please enter states. 3 char max. Max 20 states, type 'done' when finished. Then hit return:");
+                state = keyboard.nextLine();
+                state = state.trim();
+                if(state.equals("done")||i==allStates.length-1){
+                    System.out.println("Thank you for entering the states.");
+                    break;
+                }
+                else if(state.length() <=3){
+                    allStates[i]=state;
+                }
+                else{
+                    validInput=false;
+                    System.out.println("Invalid state. Program terminating");
+                }
+            }
+        }
+        System.out.println();
         //Get Accept states.
         if(validInput==true){
             System.out.println(Q_4);
             for( int i = 0; i < acceptStates.length; i++){
-                System.out.println("Please enter an accepting state. 3 char max. Max 20 accept states, type 'done' when finished. Then hit return:");
+                System.out.println("Please enter an accepting state. 3 char max. Max 20 accept states, type 'done' when finished. Must be a state from previous state you entered.Then hit return:");
                 state = keyboard.nextLine();
                 state = state.trim();
-                if(state.equals("done")||i==acceptStates.length-1){
-                    System.out.println("Thank you for entering the accept states.");
-                    break;
-                }
-                else if(state.length() <=3 || state.equals("empty")){
-                    acceptStates[i]=state;
+                if(isStateInList(state)==true){
+                    if(state.equals("done")||i==acceptStates.length-1){
+                        System.out.println("Thank you for entering the accept states.");
+                        break;
+                    }
+                    else if(state.length() <=3 || state.equals("empty")){
+                        acceptStates[i]=state;
+                    }
+                    else{
+                        validInput=false;
+                        System.out.println("Invalid accept state. Program terminating");
+                    }
                 }
                 else{
                     validInput=false;
-                    System.out.println("Invalid accept state. Program terminating");
+                    System.out.println("Accept state not in list of states. Program terminating");
+                    break;
                 }
             }
         }
         
-        //Get all states and transitions.
-        
+        //Loop through all states and get their appropriate transitions for each letter of the alphabet. 
+      
+            int numStates = countMyArray(allStates); //get me all the fucking states as a number
+            int alphabetLen = countMyArray(alphabet); //
+            for (int i = 0; i < numStates; i++){
+            transitionTable.put(allStates[i], new ArrayList<String>(Arrays.asList("garbage")));
+            ArrayList transitions = new ArrayList<String>(Arrays.asList(""));
+            for(int j = 0; j < alphabetLen; j++){
+                System.out.println("Please enter the state transition for " + alphabet[j] + " for state " + allStates[i]);
+                state = keyboard.nextLine();
+                state = state.trim();
+                transitions.add(alphabet[j] + "->" + state);
+            }
+               if(isItAnAcceptState(allStates[i])){
+                transitions.add("epsilon->" + startState);
+            }
+            transitionTable.put(allStates[i], transitions);
+        }
+        String printItAll = transitionTable.toString();
+        System.out.println(printItAll);
+    }
+    
+    public static boolean isItAnAcceptState(String state){
+        int lengthAcceptStates = countMyArray(acceptStates);
+        for(int i = 0; i < lengthAcceptStates; i++){
+            if(acceptStates[i].equals(state)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public static int countMyArray(String[] arr){
+        int count = 0;
+        for( int i = 0; i < arr.length; i++){
+            if(arr[i] != null){
+                count++;
+            }
+            else{
+                return count;
+            }
+        }
+        return count;
+    }
+    
+    /**
+     * Function isStateInList
+     * 
+     * @parameter state //desired state to see if in List of States
+     * @return true if in list, false otherwise
+     */
+    public static boolean isStateInList(String state){
+        for( int i =0; i < allStates.length; i++){
+            String allState = allStates[i];
+            if(allState != null){
+                if(allState.equals(state)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
+    /**
+     * Function printResult()
+     * 
+     * Purpose: Print NFA or DFA in 5-tuple format.
+     * 
+     */
+    public static void printResult(){
+        
+    }
+    
+    
 }
